c9d15098796a039d35b9bfd94be5ca33b9b51717
eminokic <eminokic@iastate.edu>
2021-03-15 12:44:17 -0500

    Refactoring include line.

--- hello_world/src/hello-world.cpp
+++ hello_world/src/hello-world.cpp
@@ -1,4 +1,8 @@
 #include "hello-world.hpp"
-#include "cmake_public.cpp"
+#include "cmake_public.hpp"
 
 std::string say_hello() { return std::string("Hello, CMake superbuild!"); }
+
+int call_cmake_public();
+
+std::int test_cmake_public() {return call_cmake_public();}
