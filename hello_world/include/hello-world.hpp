efe4399b7e1ff76b2049945665a0ef2e39cdf62d
tcdavis@iastate.edu <tcdavis@iastate.edu>
2021-03-27 14:24:37 -0500

    Add optional code dependancy for cmake_public

--- hello_world/include/hello-world.hpp
+++ hello_world/include/hello-world.hpp
@@ -1,4 +1,10 @@
 #pragma once
+#include <iostream>
 #include <string>
 
+#ifdef USING_CMAKEPUBLIC_
+#include <CMakePublic/cmake_public.hpp>
+#endif
+
 std::string say_hello();
+std::string say_goodbye();
--- hello_world/src/hello-world.cpp
+++ hello_world/src/hello-world.cpp
@@ -1,3 +1,24 @@
 #include "hello-world.hpp"
 
+#ifdef USING_CMAKEPUBLIC_
+
+std::string say_goodbye() {
+    int val = cmake_public::call_cmake_public();
+    std::string str = std::to_string(val);
+    return (std::string("Goodbye from CMake Public! ") + str);
+}
+
+#else
+
+std::string say_goodbye() {
+    return (std::string("Goodbye from CMake superbuild!"));
+}
+
+#endif
+
 std::string say_hello() { return std::string("Hello, CMake superbuild!"); }
+
+int main() {
+    std::cout << say_hello() << std::endl;
+    std::cout << say_goodbye() << std::endl;
+}
