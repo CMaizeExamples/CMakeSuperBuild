c818e575223389cd6290e16dee9a8f510fbc7345
TheodoreDavis <tcdavis@iastate.edu>
2021-03-31 09:23:46 -0500

    - Update .gitignore for VSCode. - Make CMakePublic Required and move #include to source file.

--- hello_world/include/hello-world.hpp
+++ hello_world/include/hello-world.hpp
@@ -2,9 +2,5 @@
 #include <iostream>
 #include <string>
 
-#ifdef USING_CMAKEPUBLIC_
-#include <CMakePublic/cmake_public.hpp>
-#endif
-
 std::string say_hello();
 std::string say_goodbye();
--- hello_world/src/hello-world.cpp
+++ hello_world/src/hello-world.cpp
@@ -1,6 +1,5 @@
 #include "hello-world.hpp"
-
-#ifdef USING_CMAKEPUBLIC_
+#include <CMakePublic/cmake_public.hpp>
 
 std::string say_goodbye() {
     int val = cmake_public::call_cmake_public();
@@ -8,17 +7,4 @@ std::string say_goodbye() {
     return (std::string("Goodbye from CMake Public! ") + str);
 }
 
-#else
-
-std::string say_goodbye() {
-    return (std::string("Goodbye from CMake superbuild!"));
-}
-
-#endif
-
 std::string say_hello() { return std::string("Hello, CMake superbuild!"); }
-
-int main() {
-    std::cout << say_hello() << std::endl;
-    std::cout << say_goodbye() << std::endl;
-}
