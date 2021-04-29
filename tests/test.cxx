f8a6ca03bc08bcde0b24589e7a8e5370f8fbdafc
TheodoreDavis <tcdavis@iastate.edu>
2021-04-29 10:27:44 -0500

    First pass at unit testing - Added test.cxx no dependency yet - Added tests/CMakeList to install executable to stage/Debug - Updated root CMakeList to add test as external project

---
+++ tests/test.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+//#include "hello-world/hello-world.hpp"
+
+int main() {
+    //std::cout << say_hello() << std::endl;
+    //std::cout << say_goodbye() << std::endl;
+    return 0;
+}
